e0f5761ea41aa018a1289803c20a11c2d31c8135
alexHooper <alexmohack@gmail.com>
2018-10-20 21:23:19 +0530

    create hostsconf module

--- project/hosts.py
+++ project/hosts.py
@@ -3,5 +3,7 @@ from django_hosts import patterns, host
 
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='www'),
-    host(r'(\w+)', 'project.urls', name='wildcard'),
+    host(r'blog', settings.ROOT_URLCONF, name='blog'),
+    host(r'admin', settings.ROOT_URLCONF, name='admin'),
+    host(r'(?!www).*', 'project.hostsconf.urls', name='wildcard'),
 )
